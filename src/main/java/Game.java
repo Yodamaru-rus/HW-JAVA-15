451b00541efe1f80c19118c0331cde71d5c59c6d
Vitkovskiy <vitkovskiy1912@mail.ru>
2025-11-08 12:00:30 +0300

    Make an implementation through Map

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,36 +1,29 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Game {
 
-    ArrayList<Player> players = new ArrayList<>();
+    HashMap<String, Player> players = new HashMap<>();
 
     public void register(Player player) {
-        players.add(player);
+        players.put(player.getName(), player);
     }
 
     public int round(String playerName1, String playerName2) {
 
-        if (findByName(playerName1) == -1) {
+        if (!players.containsKey(playerName1)) {
             throw new NotFoundException("User with id: " + playerName1 + " Not registered.");
-        } else if (findByName(playerName2) == -1) {
+        } else if (!players.containsKey(playerName2)) {
             throw new NotFoundException("User with id: " + playerName2 + " Not registered.");
         }
 
-        if (players.get(findByName(playerName1)).getStrength() > players.get(findByName(playerName2)).getStrength()) {
+        if (players.get(playerName1).getStrength() > players.get(playerName2).getStrength()) {
             return 1;
-        } else if (players.get(findByName(playerName1)).getStrength() < players.get(findByName(playerName2)).getStrength()) {
+        } else if (players.get(playerName1).getStrength() < players.get(playerName2).getStrength()) {
             return 2;
         } else {
             return 0;
         }
     }
 
-    public int findByName(String playerName) {
-        for (Player player : players) {
-            if (player.getName() == playerName) {
-                return players.indexOf(player);
-            }
-        }
-        return -1;
-    }
 }
